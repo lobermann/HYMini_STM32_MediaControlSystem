fad0350d80de07327edf478accd17e647c3e15b3
Lukas Obermann <obermann.lukas@gmail.com>
2014-04-15 14:32:09 -0600

    Testing around with the USART

--- RaspberryPi/src/main.cpp
+++ RaspberryPi/src/main.cpp
@@ -7,13 +7,31 @@
 #include <string>
 #include <string.h>
 #include <stdint.h>
+#include <list>
 
 uint16_t color_conv(char r, char g, char b)
 {
 	return ((r & 0xf8) << 8) | ((g & 0xfc) << 3) | ((b & 0xf8) >> 3);
 };
 
-int send_write_message(int fd, char type, char slot, uint16_t color, std::string text)
+std::string string_to_hex(const std::string& input)
+{
+	static const char* const lut = "0123456789ABCDEF";
+	size_t len = input.length();
+
+	std::string output;
+	output.reserve(3 * len);
+	for (size_t i = 0; i < len; ++i)
+	{
+		const unsigned char c = input[i];
+		output.push_back(lut[c >> 4]);
+		output.push_back(lut[c & 15]);
+		output.push_back(' ');
+	}
+	return output;
+}
+
+std::string send_write_message(int fd, char type, char slot, uint16_t color, std::string text)
 {
 	const char stx = 0x02;
 	const char etx = 0x03;
@@ -24,12 +42,20 @@ int send_write_message(int fd, char type, char slot, uint16_t color, std::string
 	write_header += (color >> 8) & 0xFF;
 	write_header += color & 0xFF; //In that color
 	write_header += text; //This text
-	write_header = stx + write_header + etx; //Wrap within stx and etx
-	std::cout << "Writing: " << write_header << std::endl;
+	write_header = stx + write_header + etx + '\n'; //Wrap within stx and etx
+	std::cout << "Writing: " << string_to_hex(write_header) << std::endl;
+	std::cout << "Writing: ";
+	for(std::string::iterator iter = write_header.begin(); iter != write_header.end(); ++iter)
+	{
+		std::cout << " " << *iter << " ";
+	}
+	std::cout << std::endl;
+	//return write_header;
 	int tx_len = write(fd, write_header.c_str(), write_header.size());
 	std::cout << write_header.size() << " : " << tx_len << std::endl;
-	usleep(10000);
-	return tx_len;
+	//usleep(25000);
+	return std::string("");
+	//return tx_len;
 }
 
 int main()
@@ -69,6 +95,9 @@ int main()
 
 	const char stx = 0x02;
 	const char etx = 0x03;
+
+	std::list<std::string> output_buffer;
+	bool send_ready = true;
 
     while(true)
     {
@@ -114,19 +143,25 @@ int main()
 							
 							send_write_message(uart0_filestream, 'H', char(0), color_conv(0x00, 0xFF, 0x3C), "Media Control System Ready");
 
-							send_write_message(uart0_filestream, 'S', char(1), color_conv(0xFF, 0xFF, 0xFF), "Hitradio OE3");
-							send_write_message(uart0_filestream, 'S', char(2), color_conv(0xFF, 0xFF, 0xFF), "Kronehit");
-							send_write_message(uart0_filestream, 'S', char(3), color_conv(0xFF, 0xFF, 0xFF), "FM 4");
-							send_write_message(uart0_filestream, 'S', char(4), color_conv(0xFF, 0xFF, 0xFF), "Antenne Steiermark");
-							send_write_message(uart0_filestream, 'S', char(5), color_conv(0xFF, 0xFF, 0xFF), "Pirate Radio");
-							send_write_message(uart0_filestream, 'S', char(6), color_conv(0xFF, 0xFF, 0xFF), "EVE Radio");
+							output_buffer.push_back(send_write_message(uart0_filestream, 'S', char(11), color_conv(0xFF, 0xFF, 0xFF), "Hitradio OE3"));
+							output_buffer.push_back(send_write_message(uart0_filestream, 'S', char(12), color_conv(0xFF, 0xFF, 0xFF), "Kronehit"));
+							output_buffer.push_back(send_write_message(uart0_filestream, 'S', char(13), color_conv(0xFF, 0xFF, 0xFF), "FM 4"));
+							output_buffer.push_back(send_write_message(uart0_filestream, 'S', char(14), color_conv(0xFF, 0xFF, 0xFF), "Antenne Steiermark"));
+							output_buffer.push_back(send_write_message(uart0_filestream, 'S', char(15), color_conv(0xFF, 0xFF, 0xFF), "Pirate Radio"));
+							output_buffer.push_back(send_write_message(uart0_filestream, 'S', char(16), color_conv(0xFF, 0xFF, 0xFF), "EVE Radio"));
 
 							single_msg.clear();
+						}
+						else if(single_msg.at(0) == 'A')
+						{
+							std::cout << "Setting Send Ready" << std::endl;
+							send_ready = true;
 						}
 						else
 						{
 							std::cout << "Unimplemented Message" << std::endl;
 						}
+						single_msg.clear();
 					}
 					else
 					{
@@ -136,6 +171,22 @@ int main()
 				msg_buffer.clear();
 	  		}
 	  	}
+		
+		/*if(send_ready && !output_buffer.empty())
+		{
+			send_ready = false;
+			std::string sendstring = output_buffer.front();
+			std::cout << "Writing: " << string_to_hex(sendstring) << std::endl;
+		    std::cout << "Writing: ";
+			for(std::string::iterator iter = sendstring.begin(); iter != sendstring.end(); ++iter)
+			{
+				std::cout << " " << *iter << " ";
+			}
+			std::cout << std::endl;
+			output_buffer.pop_front();
+			write(uart0_filestream, sendstring.c_str(), sendstring.size());
+		}*/
+
 		usleep(5000);
 	}
 
